3dfacdf7d3b25eb3c45dc79b1ceed269f745245a
John Singson <john.singson9@gmail.com>
2018-07-28 16:44:11 -0500

    added comments for code

--- static/js/app.js
+++ static/js/app.js
@@ -12,6 +12,7 @@ table.attr("class", "table table-striped");
 
 console.log(data);
 
+// build the initial table with the contents from data.js
 function buildTable(aliens) {
   aliens.forEach((alienSightings) => {
     var row = tbody.append("tr");
@@ -22,25 +23,33 @@ function buildTable(aliens) {
 }); 
 }
 
+// initiate the buildtable function with the data from tableData
 buildTable(tableData);
 
 
+// select the submit button in html
 var submit = d3.select("#filter-btn");
 
+// turn it on
 submit.on("click", function() {
 
+    // empty the table contents for the new table with filters
     tbody.html("")
 
+    // prevent the page from refreshing
     d3.event.preventDefault();
 
+    // set the inputs and value
     var date_input = d3.select("#datetime");
     var date_value = date_input.property("value");
 
     console.log(date_input);
     console.log(date_value);
 
+    // filter the data
     var filteredData = tableData.filter(person => person.datetime === date_value);
 
+    // create an else if statement to differentiate between the filtered data
     if(!date_value) {
         buildTable(tableData);
     }
